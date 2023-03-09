f7036fc4bdb9e95cf8d4fc78158dee877a0717ed
raushangpt <rk880488@gmail.com>
2023-03-09 22:28:47 +0530

    update

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.svelte'],
-  theme: {
-    colors: {
-      'dark-cyan': 'hsl(158, 36%, 37%)',
-      'cream': 'hsl(30, 38%, 92%)',
-      'very-dark-blue': 'hsl(212, 21%, 14%)',
-      'dark-grayish-blue': 'hsl(228, 12%, 48%)',
-      'white': 'hsl(0, 0%, 100%)',
+    content: ['./index.html'],
+    theme: {
+        colors: {
+            'dark-cyan': 'hsl(158, 36%, 37%)',
+            'cream': 'hsl(30, 38%, 92%)',
+            'very-dark-blue': 'hsl(212, 21%, 14%)',
+            'dark-grayish-blue': 'hsl(228, 12%, 48%)',
+            'white': 'hsl(0, 0%, 100%)',
+        },
+        fontFamily: {
+            'fraunces': ['Fraunces', 'serif'],
+            'montserrat': ['Montserrat', 'sans-serif'],
+        },
+        extend: {},
     },
-    fontFamily: {
-      'fraunces': ['Fraunces', 'serif'],
-      'montserrat': ['Montserrat', 'sans-serif'],
-    },
-    extend: {},
-  },
-  plugins: [],
+    plugins: [],
 };
